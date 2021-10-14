f290b0c46a4111832d236618487e12ea049be57c
nortonbatista <nortonbatistaa@gmail.com>
2021-10-14 11:19:35 -0300

    Create articles#update function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,16 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
   
   private
     def article_params
