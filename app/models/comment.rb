fa0a5d02e35eb91656550c5c0592ab9f6a782607
nortonbatista <nortonbatistaa@gmail.com>
2021-10-14 13:34:26 -0300

    Set possible states for comments and articles

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,12 @@ class Comment < ApplicationRecord
   validates :article_id, presence: true
   validates :commenter, presence: true
   validates :body, presence: true
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
