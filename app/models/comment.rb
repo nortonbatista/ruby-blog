6c2f228fb9e05755f17b4d46b114ee5a68b30a39
nortonbatista <nortonbatistaa@gmail.com>
2021-10-14 14:00:08 -0300

    Remove duplicated check and use include insted

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,9 @@
 class Comment < ApplicationRecord
+  include Visible
+  
   belongs_to :article
 
   validates :article_id, presence: true
   validates :commenter, presence: true
   validates :body, presence: true
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
