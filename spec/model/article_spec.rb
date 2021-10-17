a417322d52c90f69fefecd8f7d6d56f5866bbea2
Norton Batista Abdala <nortonbatistaa@gmail.com>
2021-10-17 15:35:46 -0300

    Create tests for article

--- spec/model/article_spec.rb
+++ spec/model/article_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
+  context "Validate article's title" do
+    it "is valid" do
+      article = Article.new(title: "New Article", body: "New Article Body")
 
+      expect(article).to be_valid
+    end
+
+    it "isn't valid" do
+      article = Article.new(body: "New Article Body")
+
+      expect(article).to_not be_valid
+    end
+  end
+
+  context "Validate article's body" do
+    it "is valid" do
+      article = Article.new(title: "New Article", body: "New Article Body")
+
+      expect(article).to be_valid
+    end
+
+    it "isn't valid" do
+      article1 = Article.new(title: "New Article", body: "")
+      article2 = Article.new(title: "New Article", body: "A") # Less than 10 characters
+
+      expect(article1).to_not be_valid
+      expect(article2).to_not be_valid
+    end
+  end
 end
